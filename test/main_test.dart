1959a9fdfef4968ec3d0f00fe0d0e78918e9dd12
André Terra <and.an.terra@gmail.com>
2022-11-06 06:50:04 -0300

    Remove code coverage

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,6 +1,7 @@
-import 'package:appointment/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("Testing CI", () => {expect(true, true)});
+  test("CI Test", () {
+    expect(true, true);
+  });
 }
