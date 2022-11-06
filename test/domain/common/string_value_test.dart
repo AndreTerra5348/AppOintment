6eb0c428b4748625d22a239f6a7894c94d542e00
André Terra <and.an.terra@gmail.com>
2022-11-06 08:20:27 -0300

    Split test groups into functions

--- test/domain/common/string_value_test.dart
+++ test/domain/common/string_value_test.dart
@@ -4,50 +4,55 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group("String value validator", () {
-    test(
-        "StringLenValidator Should return Right(value) when validate is call with a string whose length is less than len parameter",
-        () {
-      // Arrange
-      String value = "test";
+  group("String Value Validators", () {
+    group("StringLenValidator", stringLenValidatorTests);
+  });
+}
 
-      Validator<StringFailure, String> sut = StringLenValidator(5);
+void stringLenValidatorTests() {
+  test(
+      "Should return Right(value) when validate is call with a string whose length is less than len parameter",
+      () {
+    // Arrange
+    String value = "test";
+    int length = 5;
 
-      // Act
-      var actual = sut(value);
+    Validator<StringFailure, String> sut = StringLenValidator(length);
 
-      // Assert
-      expect(actual, Right(value));
-    });
+    // Act
+    var actual = sut(value);
 
-    test(
-        "StringLenValidator Should return Left(StringFailure.exceedingLength) when validate is call with a string whose length is equal to len parameter",
-        () {
-      // Arrange
-      String value = "test";
+    // Assert
+    expect(actual, Right(value));
+  });
 
-      Validator<StringFailure, String> sut = StringLenValidator(4);
+  test(
+      "Should return Left(StringFailure.exceedingLength) when validate is call with a string whose length is equal to len parameter",
+      () {
+    // Arrange
+    String value = "test";
+    int length = 4;
+    Validator<StringFailure, String> sut = StringLenValidator(length);
 
-      // Act
-      var actual = sut(value);
+    // Act
+    var actual = sut(value);
 
-      // Assert
-      expect(actual, const Left(StringFailure.exceedingLength()));
-    });
+    // Assert
+    expect(actual, const Left(StringFailure.exceedingLength()));
+  });
 
-    test(
-        "StringLenValidator Should return Left(StringFailure.exceedingLength) when validate is call with a string whose length greater than len parameter",
-        () {
-      // Arrange
-      String value = "test";
+  test(
+      "Should return Left(StringFailure.exceedingLength) when validate is call with a string whose length is greater than len parameter",
+      () {
+    // Arrange
+    String value = "test";
+    int length = 2;
+    Validator<StringFailure, String> sut = StringLenValidator(length);
 
-      Validator<StringFailure, String> sut = StringLenValidator(2);
+    // Act
+    var actual = sut(value);
 
-      // Act
-      var actual = sut(value);
-
-      // Assert
-      expect(actual, const Left(StringFailure.exceedingLength()));
-    });
+    // Assert
+    expect(actual, const Left(StringFailure.exceedingLength()));
   });
 }
