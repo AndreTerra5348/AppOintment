d6734537cdbb0e6470e7feb737fc5e97d43895c1
André Terra <and.an.terra@gmail.com>
2022-11-09 09:00:03 -0300

    Change uid failure file

--- lib/domain/common/failures.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'failures.freezed.dart';
-
-@freezed
-class UidFailure with _$UidFailure {
-  const factory UidFailure.invalid() = InvalidUid;
-}
--- lib/domain/common/values.dart
+++ lib/domain/common/values.dart
@@ -1,6 +1,8 @@
-import 'package:appointment/domain/common/failures.dart';
 import 'package:appointment/domain/common/value_object.dart';
 import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'values.freezed.dart';
 
 class Uid extends ValueObject<UidFailure, int> {
   @override
@@ -9,4 +11,9 @@ class Uid extends ValueObject<UidFailure, int> {
   const Uid._(this.value);
   factory Uid() => const Uid._(Left(UidFailure.invalid()));
   factory Uid.fromInt(int id) => Uid._(Right(id));
+}
+
+@freezed
+class UidFailure with _$UidFailure {
+  const factory UidFailure.invalid() = InvalidUid;
 }
