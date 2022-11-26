7c51917a2308f95a7b52575ee2c2682391596875
Andre Terra <andre.terra@bcmautomacao.com.br>
2022-11-26 07:16:17 -0300

    Add EntityMixin validity property

--- lib/domain/common/entity_mixin.dart
+++ lib/domain/common/entity_mixin.dart
@@ -1,5 +1,6 @@
 import 'package:appointment/domain/common/value_object.dart';
 import 'package:appointment/domain/common/values.dart';
+import 'package:dartz/dartz.dart';
 
 mixin EntityMixin {
   bool get isValid => values.every((element) => element.isValid);
@@ -7,5 +8,10 @@ mixin EntityMixin {
   bool get isNotValid => !isValid;
 
   List<ValueObject<dynamic, dynamic>> get values;
+
+  Option<EntityMixin> get validity {
+    return values.every((element) => element.isValid) ? some(this) : none();
+  }
+
   Uid get id;
 }
