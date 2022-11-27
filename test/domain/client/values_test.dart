81d037cd9339c34eb1c5de8635f06a350925a99d
Andre Terra <andre.terra@bcmautomacao.com.br>
2022-11-27 07:22:30 -0300

    Update ValueObject test descriptions

--- test/domain/client/values_test.dart
+++ test/domain/client/values_test.dart
@@ -1,12 +1,43 @@
 import 'package:appointment/domain/client/values.dart';
+import 'package:appointment/domain/common/error.dart';
 import 'package:appointment/domain/common/failures.dart';
-import 'package:appointment/domain/common/value_object.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("Value Object", () {
-    group("Name", nameTests);
+    group("Name", () {
+      test(
+          "Should return Right(value) "
+          "when name length is equal to 1", () {
+        // Arrange
+        const name = "B";
+        expectRight(name);
+      });
+      test(
+          "Should return a type of Left<StringFailure, String>, String> "
+          "when name length is less than 1", () {
+        // Arrange
+        const name = "";
+        expectLeft(name);
+      });
+
+      test(
+          "Should return Right(value) "
+          "when name contains accents", () {
+        // Arrange
+        const name = "André";
+        expectRight(name);
+      });
+
+      test(
+          "Should return a type of Left<StringFailure, String>, String> "
+          "when name contains number", () {
+        // Arrange
+        const name = "A0";
+        expectLeft(name);
+      });
+    });
   });
 }
 
@@ -34,34 +65,5 @@ void expectLeft(String name) {
   expect(actual, isA<Left<StringFailure, String>>());
   expect(sut.isValid, isFalse);
   expect(sut.isNotValid, isTrue);
-  expect(() => sut.getOrThrow(), throwsA(isA<UnexpectedValueError>()));
-}
-
-void nameTests() {
-  test("Should return Right(value), when name length is equal to 1", () {
-    // Arrange
-    const name = "B";
-    expectRight(name);
-  });
-  test(
-      "Should return a type of Left<StringFailure, String>, String> when name length is less than 1",
-      () {
-    // Arrange
-    const name = "";
-    expectLeft(name);
-  });
-
-  test("Should return Right(value) when name contains accents", () {
-    // Arrange
-    const name = "André";
-    expectRight(name);
-  });
-
-  test(
-      "Should return a type of Left<StringFailure, String>, String> when name contains number",
-      () {
-    // Arrange
-    const name = "A0";
-    expectLeft(name);
-  });
+  expect(() => sut.getOrThrow(), throwsA(isA<CriticalError>()));
 }
