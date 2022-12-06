8b450beabae26ed57f9978444253c96cc85ce597
André Terra <and.an.terra@gmail.com>
2022-12-06 17:42:57 -0300

    Remove Injectable package

--- lib/infrastructure/drift/db.dart
+++ lib/infrastructure/drift/db.dart
@@ -3,13 +3,11 @@ import 'dart:io';
 import 'package:appointment/infrastructure/client/table.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:injectable/injectable.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
 part 'db.g.dart';
 
-@injectable
 @DriftDatabase(tables: [ClientModels])
 class DriftDb extends _$DriftDb {
   DriftDb({required QueryExecutor executor}) : super(executor);
@@ -17,9 +15,8 @@ class DriftDb extends _$DriftDb {
   int get schemaVersion => 1;
 }
 
-@module
-abstract class QueryExecutorProvider {
-  QueryExecutor get nativeDB => LazyDatabase(() async {
+class QueryExecutorProvider {
+  static QueryExecutor get nativeDB => LazyDatabase(() async {
         final dbFolder = await getApplicationDocumentsDirectory();
         final file = File(p.join(dbFolder.path, 'db.sqlite'));
         return NativeDatabase(file);
