07b0ec2c02606d9a7c6fba1ac2ef60fd25810b94
André Terra <and.an.terra@gmail.com>
2022-12-06 14:07:31 -0300

    Add PageServiceFailure to failure_fixture

--- test/common/failure_fixture.dart
+++ test/common/failure_fixture.dart
@@ -1,5 +1,6 @@
 import 'package:appointment/application/common/form.dart';
 import 'package:appointment/domain/core/i_repository.dart';
+import 'package:appointment/infrastructure/core/i_page_service.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations_en.dart';
 
 const dbErrorMessage = "Error";
@@ -13,3 +14,6 @@ const SubmissionStatus dbErrorSubmissionStatus = SubmissionStatus.failure(
 
 final dbErrorLocalizedMessage =
     AppLocalizationsEn().databaseFailure(dbErrorMessage);
+
+const PageServiceFailure dbErrorPageServiceFailure =
+    PageServiceFailure.dbException(error: dbErrorMessage);
