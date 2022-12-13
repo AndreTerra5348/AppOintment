cceca2878180debdfaae39f3a6417d8d2d26b3fc
André Terra <and.an.terra@gmail.com>
2022-12-13 15:23:04 -0300

    Remove HomeDrawer from HomePage

--- lib/presentation/home/page.dart
+++ lib/presentation/home/page.dart
@@ -1,6 +1,5 @@
 import 'package:appointment/presentation/common/build_context_extensions.dart';
 import 'package:appointment/presentation/config/route.dart';
-import 'package:appointment/presentation/home/widgets/drawer.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
@@ -12,17 +11,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const HomeDrawer(),
       body: Stack(
         children: [
           Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const SizedBox(height: 8),
+                const SizedBox(height: 32),
                 Expanded(
                   flex: 1,
                   child: Text(
+                    textAlign: TextAlign.center,
                     context.tr.appointmentSubtitle,
                     style: Theme.of(context).textTheme.headline4,
                   ),
