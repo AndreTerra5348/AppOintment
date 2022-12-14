904245bbc8b912ec80ae82aac20f8b76e72e50e9
André Terra <and.an.terra@gmail.com>
2022-12-14 14:52:15 -0300

    Update ClientModels to use converters

--- lib/infrastructure/client/table.dart
+++ lib/infrastructure/client/table.dart
@@ -1,6 +1,14 @@
+import 'package:appointment/infrastructure/common/type_converter.dart';
 import 'package:drift/drift.dart';
 
 class ClientModels extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text().withLength(min: 1, max: 32)();
+  IntColumn get id => integer().autoIncrement().map(UidConverter())();
+  TextColumn get name => text()
+      .withLength(
+        min: 1,
+        max: 32,
+      )
+      .map(
+        NameConverter(),
+      )();
 }
--- lib/infrastructure/drift/db.dart
+++ lib/infrastructure/drift/db.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:appointment/domain/client/values.dart';
+import 'package:appointment/domain/common/values.dart';
 import 'package:appointment/infrastructure/client/table.dart';
+import 'package:appointment/infrastructure/common/type_converter.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 part 'db.g.dart';
 
