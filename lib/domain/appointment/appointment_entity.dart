301195034b6b34869508401680dcb7b350e40bbf
André Terra <and.an.terra@gmail.com>
2023-01-18 21:40:56 -0300

    Add Appointment entity

---
+++ lib/domain/appointment/appointment_entity.dart
@@ -0,0 +1,32 @@
+/// Domain model Appointment definition.
+import 'package:appointment/domain/common/common_values.dart';
+import 'package:appointment/domain/common/entity_mixin.dart';
+import 'package:appointment/domain/common/value_object.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'appointment_entity.freezed.dart';
+
+/// Represents a [Appointment] in the domain model.
+@freezed
+class Appointment with _$Appointment, EntityMixin {
+  const Appointment._();
+
+  /// Creates a new [Appointment] with a [dateTimeRange] and an [id].
+  const factory Appointment({
+    required Identifier id,
+    required DateTimeRange dateTimeRange,
+  }) = _Appointment;
+
+  /// Creates a new [Appointment] with a [dateTimeRange] and an empty [id].
+  factory Appointment.withoutIdentifier(
+      {required DateTimeRange dateTimeRange}) {
+    return Appointment(
+      id: Identifier(),
+      dateTimeRange: dateTimeRange,
+    );
+  }
+
+  /// [List] of all [Appointment] [ValueObject]s.
+  @override
+  List<ValueObject> get values => [id, dateTimeRange];
+}
