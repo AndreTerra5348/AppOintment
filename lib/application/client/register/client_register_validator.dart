c1f7a35eeb2c3f5e4c9b068c7be77d61d36558fe
André Terra <and.an.terra@gmail.com>
2023-01-27 13:07:14 -0300

    Change RegisterValidator to be more generic

--- lib/application/client/register/client_register_validator.dart
+++ lib/application/client/register/client_register_validator.dart
@@ -1,19 +1,12 @@
 /// Defines [ClientRegisterValidator]
 import 'package:appointment/application/register/register_validator.dart';
 import 'package:appointment/domain/client/client_entity.dart';
-import 'package:dartz/dartz.dart';
+import 'package:appointment/domain/common/value_object.dart';
 
 /// Validates the [Client] entity [ValueObject]s needed for registration
-class ClientRegisterValidator implements RegisterValidator<Client> {
-  /// Validates the [Client] entity
-  /// returns [Some] if the entity is valid
-  /// returns [None] if the entity is invalid
+class ClientRegisterValidator with RegisterValidator<Client> {
   @override
-  Option<Client> validate(Client entity) {
-    return [
-      entity.name,
-    ].every((element) => element.isValid)
-        ? some(entity)
-        : none();
+  List<ValueObject> getValues(Client entity) {
+    return [entity.name];
   }
 }
--- lib/application/register/register_validator.dart
+++ lib/application/register/register_validator.dart
@@ -1,9 +1,17 @@
 /// Defines [RegisterValidator]
 import 'package:appointment/domain/common/entity_mixin.dart';
+import 'package:appointment/domain/common/value_object.dart';
 import 'package:dartz/dartz.dart';
 
 /// Validates entities value objects needed for registration
 abstract class RegisterValidator<T extends EntityMixin> {
+  /// A [List] of all the [ValueObject] of the entity, needed for registration.
+  List<ValueObject<dynamic, dynamic>> getValues(T entity);
+
   /// Validates the entity
-  Option<T> validate(T entity);
+  Option<T> validate(T entity) {
+    return getValues(entity).every((element) => element.isValid)
+        ? some(entity)
+        : none();
+  }
 }
